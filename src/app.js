db124d3199cfa8b730eabb79123328564258f2cd
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-21 17:36:14 -0300

    putting the db into operation

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ const dotenv = require('dotenv');
 
 const app = express();
 
+const db = require('./database/database');
+
 dotenv.config({ path: 'variables.env' });
+
+db.authenticate()
+    .then(() => console.log('Connection has been established successfully'))
+    .catch((error) => console.log(error));
 
 app.use(express.json());
 
