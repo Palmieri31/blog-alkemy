0004a6f79cb3dec417face96db4a30b827d8db9a
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-23 12:59:07 -0300

    validating that the URL contains an image

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,9 +1,16 @@
 const { body, validationResult } = require('express-validator');
+const isImageURL = require('image-url-validator').default;
 
 module.exports.postValidationRules = () => [
     body('title', 'Your title is empty').not().isEmpty(),
     body('content', 'Your content is empty').not().isEmpty(),
     body('image', 'Your image is empty').not().isEmpty(),
+    body('image').custom( async (value) => {
+        const result = await isImageURL(value);
+        if(!result) {
+            throw new Error('it is not an image');
+        }
+    }),
     body('categoryId', 'Your categoryId is empty').not().isEmpty(),
     body('categoryId', 'Your categoryId is not a number').isNumeric(),
     body('creation_date', 'Your creation_date is empty').not().isEmpty(),
